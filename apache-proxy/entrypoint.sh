4fba8c2e7b23e706a418a2a67d9c0dc57e0d40a7
Christian Klauenbosch <christian@klauenboesch.ch>
2017-08-23 11:06:23 +0200

    removed incron, improved certbot script

--- apache-proxy/entrypoint.sh
+++ apache-proxy/entrypoint.sh
@@ -2,7 +2,7 @@
 
 service apache2 stop
 letsencrypt register --agree-tos -m $NOTIFY_MAIL --non-interactive
-certbot --renew
+certbot --renew-with-new-domains
 service apache2 restart
 cron
 tail -F /var/log/apache2/*
